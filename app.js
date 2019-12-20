6bddbfa53945f8a95e61e3d6e91367946e25a247
hefang <hefang.com>
2019-12-20 14:58:08 +0800

    blog和user接口初步搭建

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
+const handleBlogRouter = require('./src/router/blog')
+const handleUserRouter = require('./src/router/user')
 const serverHandle = (req,res) => {
     // 设置响应格式json
     res.setHeader('Content-Type", "application/json')
 
-    const resData = {
-        name:'何方',
-        site:'moon',
-        env: process.env.NODE_ENV
+    // 处理blog路由
+    const blogData = handleBlogRouter(req,res)
+    if (blogData) {
+        res.end(JSON.stringify(blogData))
+        return
     }
 
-    res.end(
-        JSON.stringify(resData)
-    )
+    // 处理user路由
+    const userData = handleBlogRouter(req,res)
+    if (userData) {
+        res.end(JSON.stringify(userData))
+        return
+    }
+
+    // 未匹配到路由，返回 404
+    res.writeHead(404, {"Content-type": "text/plain"})
+    res.write("404 Not Found\n")
+    res.end()
 }
 
 module.exports = serverHandle
